76f19523606395e94dd1623509ec779e23573176
studymachine <1771134505@qq.com>
2022-02-13 15:04:30 +0800

    新建模板

---
+++ src/print.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: studymachine
+ * @Date: 2022-02-13 13:12:39
+ * @LastEditors: studymachine
+ * @LastEditTime: 2022-02-13 13:13:42
+ * @Description: file content
+ */
+#include "print.h"
+
+void printAll();
---
+++ src/print.h
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+void printAll() {
+    cout << "nihao" << endl;
+}

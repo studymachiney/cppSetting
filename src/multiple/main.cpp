b7d621381eb2e407970093673d3b3a0234aa6fea
studymachine <1771134505@qq.com>
2022-02-13 16:31:46 +0800

    Edit Template

---
+++ src/multiple/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+#include <string>
+#include <vector>
+
+void printAll();
+
+int main(int, char**) {
+    struct student {
+        string name;
+        int age;
+        int score;
+    };
+    student s = { "张三", 18, 88};
+    int a = 5;
+    string str = "nihao";
+    char charlist[] = "list";
+    cout << sizeof(long) << endl;
+    int b[] = {1,2,3,4,52};
+    vector<int> aa = {1,2,3};
+    a++;
+    cout << a << endl;
+    cout << "name: " << s.name << " age: " << s.age << " score " << s.score << endl;
+    cout << "Hello, world!\n";
+    printAll();
+    system("pause");
+    return 0;
+}

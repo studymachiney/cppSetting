4b06eae42cc3a2db1a0f85bd2856f09f9e23632a
studymachine <1771134505@qq.com>
2022-03-14 22:00:35 +0800

    堆排序实践

--- study/chapter01/6.heapSort.cpp
+++ study/chapter01/6.heapSort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 //堆排序
 //某个数出现在index位置，往上移动
@@ -47,10 +48,34 @@ void heapSort(vector<int> &arr)
         swap(arr[0], arr[--heapSize]);
     }
 }
+
+//已知一个几乎有序的数组，几乎有序是指，如果把数组排好顺序的话，每个元素移动的距离可以不超过k，并且k相对于数组来说比较小。请选择一个合适的排序算法针对这个数据进行排序。
+void sortedArrDistanceLessK(vector<int> &arr, int k)
+{
+    //优先级队列
+    // priority_queue<int, vector<int>, less<int>> heap;//不写第三个参数或者写成less都是大根堆
+    priority_queue<int, vector<int>, greater<int>> heap; //greater是小根堆
+    int index = 0;
+    for (; index <= min((int)(arr.size()), k); index++)
+    {
+        heap.push(arr[index]);
+    }
+    int i=0;
+    for(;index<arr.size();i++,index++) {
+        heap.push(arr[index]);
+        arr[i] = heap.top();
+        heap.pop();
+    }
+    while(!heap.empty()) {
+        arr[i++] = heap.top();
+        heap.pop();
+    }
+}
 int main()
 {
-    vector<int> arr{1, 24, 6, 3, 67, 54, 234, 5, 574, 32, 563, 5, 3, 7, 5, 66, 74};
-    heapSort(arr);
+    vector<int> arr{1,5,3,2,7,98,77,34,65};
+    // heapSort(arr);
+    sortedArrDistanceLessK(arr, 4);
     for (int item : arr)
     {
         cout << item << " ";
